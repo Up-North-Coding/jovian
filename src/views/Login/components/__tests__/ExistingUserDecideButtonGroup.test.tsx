70a11d878fd14fdb29cfc9aeea5621d516f35d9d
outragedhuman <outragedhumangithub@mm.st>
2022-04-08 16:44:16 -0500

    remove existinguserdecidebuttongroup toggle test

--- src/views/Login/components/__tests__/ExistingUserDecideButtonGroup.test.tsx
+++ src/views/Login/components/__tests__/ExistingUserDecideButtonGroup.test.tsx
@@ -1,28 +1,14 @@
-import React from "react";
 import ReactDOM from "react-dom";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import ExistingUserDecideButtonGroup from "../ExistingUserDecideButtonGroup";
-
-let toggleFnSpy: Function;
-beforeEach(() => {
-  toggleFnSpy = jest.fn();
-});
 
 it("should render without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<ExistingUserDecideButtonGroup toggleFn={toggleFnSpy} />, div);
+  ReactDOM.render(<ExistingUserDecideButtonGroup />, div);
 });
 
 it("should contain a New User and Existing User button", () => {
-  render(<ExistingUserDecideButtonGroup toggleFn={toggleFnSpy} />);
+  render(<ExistingUserDecideButtonGroup />);
   expect(screen.getByText("New User")).toBeInTheDocument();
   expect(screen.getByText("Existing User")).toBeInTheDocument();
-});
-
-it("should call toggleFn onClick", () => {
-  render(<ExistingUserDecideButtonGroup toggleFn={toggleFnSpy} />);
-  expect(screen.getByText("Existing User")).toBeInTheDocument();
-  fireEvent.click(screen.getByText("Existing User"));
-
-  expect(toggleFnSpy).toHaveBeenCalledTimes(1);
 });
