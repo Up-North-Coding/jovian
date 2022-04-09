46b796675bc7cae4db3a28eb497e3274b0b1f8d7
outragedhuman <outragedhumangithub@mm.st>
2022-04-09 16:22:33 -0500

    fix function types

--- src/contexts/AccountContext/types.ts
+++ src/contexts/AccountContext/types.ts
@@ -1,8 +1,6 @@
 export interface ContextValues {
   accountRs?: string;
   accountSeed?: string;
-
-  // CR: define the function args here as a best practice along with it's return type instead of 'Function'
-  fetchFn?: Function;
-  flushFn?: Function;
+  fetchFn?(): void;
+  flushFn?(): void;
 }
