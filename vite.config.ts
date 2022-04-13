7f6c4d4961bb90b6019f52ee252f1881d1fbf52f
outragedhuman <outragedhumangithub@mm.st>
2022-04-12 20:34:32 -0500

    disable checker (it's crashing)

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import checker from "vite-plugin-checker"; // error overlays when build issues arrise
+// import checker from "vite-plugin-checker"; // error overlays when build issues arrise
 import istanbul from "vite-plugin-istanbul";
 
 export default defineConfig(({ command, mode }) => {
@@ -18,16 +18,16 @@ export default defineConfig(({ command, mode }) => {
     plugins: [
       react(),
       tsconfigPaths(),
-      checker({
-        typescript: true,
-        eslint: {
-          // NOTE: eslint --fix could crash the build server on errors
-          lintCommand: 'eslint --config ".eslintrc.json" --config ".eslint-dev-rules.json" "./src/" --fix',
-        },
-        overlay: {
-          initialIsOpen: true,
-        },
-      }),
+      // checker({
+      //   typescript: true,
+      //   eslint: {
+      //     // NOTE: eslint --fix could crash the build server on errors
+      //     lintCommand: 'eslint --config ".eslintrc.json" --config ".eslint-dev-rules.json" "./src/" --fix',
+      //   },
+      //   overlay: {
+      //     initialIsOpen: true,
+      //   },
+      // }),
       istanbul({
         include: "src/*",
         exclude: [
