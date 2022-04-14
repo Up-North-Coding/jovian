a92a6135271ff560ae75d988b3ba8c0134cdd302
outragedhuman <outragedhumangithub@mm.st>
2022-04-14 18:47:34 -0500

    add elements from layout sketch and get basic onClick logs working

--- src/views/Dashboard/components/AddressBook/AddressBook.tsx
+++ src/views/Dashboard/components/AddressBook/AddressBook.tsx
@@ -1,5 +1,18 @@
-import React, { useEffect, useMemo, useState } from "react";
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Stack, Typography } from "@mui/material";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  FormControl,
+  FormGroup,
+  Input,
+  Stack,
+  styled,
+  Typography,
+} from "@mui/material";
 import useBreakpoint from "hooks/useBreakpoint";
 
 // const style = {
@@ -14,6 +27,58 @@ import useBreakpoint from "hooks/useBreakpoint";
 //   p: 4,
 // };
 
+// [ ]: "Add / +" button
+// Expands into input + "Add" button
+// [ ]: Tests
+// [ ]: Close button
+// [ ]: Put address rows into a table of some sort
+// [x] Done button
+
+const AddNewAddressInput: React.FC = () => {
+  const [isEnterAddressMode, setIsEnterAddressMode] = useState<boolean>(false);
+  const [newAddress, setNewAddress] = useState<string>();
+
+  const toggleAddressMode = useCallback(() => {
+    console.log("entering add address mode...");
+    setIsEnterAddressMode((prev) => !prev);
+  }, []);
+
+  const handleAddAddress = useCallback(() => {
+    console.log("adding new address...", newAddress);
+    const inputType = checkInputType(newAddress);
+    if (inputType === "account") {
+      console.log("adding based on account type...");
+    } else if (inputType === "alias") {
+      console.log("adding based on alias type...");
+    }
+  }, [newAddress]);
+
+  const handleNewAddressEntry = useCallback((inputVal: string) => {
+    setNewAddress(inputVal);
+  }, []);
+
+  useEffect(() => {
+    console.log("input:", newAddress);
+  }, [newAddress]);
+
+  return (
+    <>
+      {isEnterAddressMode ? (
+        <FormGroup row>
+          <Input onChange={(e) => handleNewAddressEntry(e.target.value)} placeholder="Enter address or alias"></Input>
+          <Button onClick={handleAddAddress} variant="contained">
+            Add
+          </Button>
+        </FormGroup>
+      ) : (
+        <Button onClick={toggleAddressMode} variant="outlined">
+          +
+        </Button>
+      )}
+    </>
+  );
+};
+
 const AddressBook: React.FC = () => {
   const [open, setOpen] = React.useState(false);
   const fullScreen = useBreakpoint("<", "md");
@@ -23,24 +88,33 @@ const AddressBook: React.FC = () => {
     setAddressList(["JUP-ABCD-ABCD-ABCD-EFGHJ", "JUP-XXXX-XXXX-XXXX-XXXXX", "JUP-AAAA-BBBB-CCCC-DDDDD"]);
   }, []);
 
-  const handleClickOpen = () => {
+  const handleClickOpen = useCallback(() => {
     setOpen(true);
-  };
+  }, []);
 
-  const handleClose = () => {
+  const handleAddressDelete = useCallback((event, accountToDelete: string) => {
+    console.log("delete address:", event, "account to delete:", accountToDelete);
+  }, []);
+
+  const handleSendToAddress = useCallback((event, accountToSendTo: string) => {
+    console.log("sent to address:", event, "account to send to:", accountToSendTo);
+  }, []);
+
+  const handleClose = useCallback(() => {
     setOpen(false);
-  };
+  }, []);
+
   const Addresses = useMemo(() => {
     return addressList?.map((accountRs) => {
       return (
         <Stack key={accountRs} direction={"row"}>
           <Typography>{accountRs}</Typography>
-          <Button>Del</Button>
-          <Button>Send</Button>
+          <Button onClick={(e) => handleAddressDelete(e, accountRs)}>Del</Button>
+          <Button onClick={(e) => handleSendToAddress(e, accountRs)}>Send</Button>
         </Stack>
       );
     });
-  }, [addressList]);
+  }, [addressList, handleAddressDelete, handleSendToAddress]);
 
   return (
     <div>
@@ -48,7 +122,11 @@ const AddressBook: React.FC = () => {
         Address Book
       </Button>
       <Dialog fullScreen={fullScreen} open={open} onClose={handleClose}>
+        <StyledCloseButton onClick={handleClose} variant="outlined">
+          X
+        </StyledCloseButton>
         <DialogTitle>{"Address Book"}</DialogTitle>
+        <AddNewAddressInput />
         <DialogContent>
           <DialogContentText>{Addresses}</DialogContentText>
         </DialogContent>
@@ -61,6 +139,36 @@ const AddressBook: React.FC = () => {
     </div>
   );
 };
+
+const StyledCloseButton = styled(Button)(({ theme }) => ({
+  width: "5%",
+  margin: theme.spacing(2),
+}));
+
+//
+// Helper functions
+//
+
+function checkInputType(text?: string) {
+  if (text === undefined) {
+    return;
+  }
+  console.log("checking input string:", text, "setting type based on regex matches");
+  const JUPRegex = /JUP-/i;
+  const ALIASRegex = /\w/i;
+
+  // Entry is likely an account/address
+  if (JUPRegex.test(text)) {
+    console.log("entry is likely an account, returning 'account'");
+    return "account";
+
+    // Entry is likely an alias
+    // TODO: improve detection
+  } else if (ALIASRegex.test(text)) {
+    console.log("entry is likely an alias, returning 'alias'");
+    return "alias";
+  }
+}
 
 export default React.memo(AddressBook);
 
