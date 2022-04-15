1ca524134b55f76ab1342a3454002df842f46b82
outragedhuman <outragedhumangithub@mm.st>
2022-04-14 23:10:25 -0500

    add cypress test

--- cypress/integration/dashboard.spec.ts
+++ cypress/integration/dashboard.spec.ts
@@ -12,6 +12,7 @@
 // [ ] Clicking "SEND" button should open send modal with appropriate address entered in "TO" field
 // [ ] Entering an invalid address in the "add" input should be rejected
 // [ ] Entering an alias into the "add" input should fetch the address or be rejected
+// [x] Entering a duplicate JUP- address should be rejected
 
 describe("address book", () => {
   Cypress.Promise.onPossiblyUnhandledRejection((error, promise) => {
@@ -65,5 +66,17 @@ describe("address book", () => {
     cy.get(".MuiTableBody-root > .MuiTableRow-root > th.MuiTableCell-root").should("contain.text", testAddy);
     cy.get("button").contains("Del").click();
     cy.get(".MuiTableBody-root").should("not.contain.text", testAddy);
+  });
+
+  it.only("should save a JUP- address when entered correctly and reject the same address a second time", () => {
+    const testAddy = "JUP-ABCD-ABCD-ABCD-ABCDE";
+    cy.get("button").contains("+").click();
+    cy.get('input[placeholder*="Enter address or alias"]').type(testAddy);
+    cy.get("button").contains(/^add$/i).click({ force: true }); //TODO: fix force
+    cy.get(".MuiTableBody-root > .MuiTableRow-root > th.MuiTableCell-root").should("contain.text", testAddy);
+
+    // re-add the same address again
+    cy.get("button").contains(/^add$/i).click({ force: true }); //TODO: fix force
+    cy.get(".MuiTableBody-root > .MuiTableRow-root > th.MuiTableCell-root").should("contain.text", testAddy);
   });
 });
