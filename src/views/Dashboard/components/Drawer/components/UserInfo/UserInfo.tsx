0c969cfe79c769d349dd9eb90503331c3aa8f26a
outragedhuman <outragedhumangithub@mm.st>
2022-04-27 20:26:27 -0500

    allow copy to clipboard

--- src/views/Dashboard/components/Drawer/components/UserInfo/UserInfo.tsx
+++ src/views/Dashboard/components/Drawer/components/UserInfo/UserInfo.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from "react";
+import React, { memo, useCallback } from "react";
 import { Avatar, Chip } from "@mui/material";
 import useAccount from "hooks/useAccount";
 
@@ -7,12 +7,23 @@ const placeholderBalance = 10;
 const UserInfo: React.FC = () => {
   const { accountRs, accountName } = useAccount();
 
+  const handleCopy = useCallback(
+    (toCopy: string | undefined) => {
+      if (accountRs === undefined || toCopy === undefined) {
+        return;
+      }
+      // TODO: consider IE support
+      navigator.clipboard.writeText(toCopy);
+    },
+    [accountRs]
+  );
+
   return (
     <>
-      <Chip label={accountRs} avatar={<Avatar />} />
+      <Chip label={accountRs} avatar={<Avatar />} onClick={() => handleCopy(accountRs)} />
       {/* TODO: Add tooltip explaining what an accountName is for */}
-      <Chip size="small" label={accountName} />
-      <Chip size="small" label={placeholderBalance + " JUP"} />
+      <Chip size="small" label={accountName} onClick={() => handleCopy(accountName)} />
+      <Chip size="small" label={placeholderBalance + " JUP"} onClick={() => handleCopy(placeholderBalance.toString())} />
     </>
   );
 };
