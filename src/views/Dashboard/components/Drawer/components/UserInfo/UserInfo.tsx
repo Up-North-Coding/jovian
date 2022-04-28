c7dfce633f025211bb78c535cf443b6a9785070c
outragedhuman <outragedhumangithub@mm.st>
2022-04-27 21:07:12 -0500

    overall styling improvements

--- src/views/Dashboard/components/Drawer/components/UserInfo/UserInfo.tsx
+++ src/views/Dashboard/components/Drawer/components/UserInfo/UserInfo.tsx
@@ -1,5 +1,5 @@
 import React, { memo, useCallback } from "react";
-import { Avatar, Chip } from "@mui/material";
+import { Avatar, Chip, styled } from "@mui/material";
 import useAccount from "hooks/useAccount";
 
 const placeholderBalance = 10;
@@ -20,12 +20,36 @@ const UserInfo: React.FC = () => {
 
   return (
     <>
-      <Chip label={accountRs} avatar={<Avatar />} onClick={() => handleCopy(accountRs)} />
+      <AccountAvatarChip label={accountRs} avatar={<Avatar />} onClick={() => handleCopy(accountRs)} />
       {/* TODO: Add tooltip explaining what an accountName is for */}
-      <Chip size="small" label={accountName} onClick={() => handleCopy(accountName)} />
-      <Chip size="small" label={placeholderBalance + " JUP"} onClick={() => handleCopy(placeholderBalance.toString())} />
+      <AccountNameChip
+        sx={{
+          minWidth: "120px",
+          borderRadius: "6px",
+          border: "1px solid white",
+        }}
+        size="small"
+        label={accountName}
+        onClick={() => handleCopy(accountName)}
+      />
+      <AccountBalanceChip size="small" label={placeholderBalance + " JUP"} onClick={() => handleCopy(placeholderBalance.toString())} />
     </>
   );
 };
+
+const AccountAvatarChip = styled(Chip)(({ theme }) => ({
+  width: "95%",
+  margin: theme.spacing(2) + " 10px",
+}));
+
+const AccountNameChip = styled(Chip)(({ theme }) => ({
+  width: "90%",
+  margin: "0px " + theme.spacing(2),
+}));
+
+const AccountBalanceChip = styled(Chip)(({ theme }) => ({
+  width: "90%",
+  margin: "0px " + theme.spacing(2),
+}));
 
 export default memo(UserInfo);
