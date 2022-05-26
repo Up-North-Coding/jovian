4238a262c45514bf16c24f96e038de1f06dba170
outragedhuman <outragedhumangithub@mm.st>
2022-05-26 18:55:45 -0500

    fixes #26, improves snackbar notifications for various actions, small cleanups

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { render } from "react-dom";
 import App from "./App";
-
 import { CssBaseline } from "@mui/material";
+
+// Snackbar/notification stuff
+import { SnackbarProvider } from "notistack";
+import { MaximumSnackbarMessages } from "utils/common/constants";
 
 render(
   <React.StrictMode>
     <CssBaseline />
-    <App />
+    <SnackbarProvider maxSnack={MaximumSnackbarMessages}>
+      <App />
+    </SnackbarProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/utils/common/messages.ts
@@ -0,0 +1,24 @@
+//
+// Snackbar message text
+//
+
+export const messageText = {
+  // Transaction related messages
+  transaction: {
+    success: "Transaction Sent",
+    failure: "Transaction Failed",
+    cancel: "Transaction Canceled",
+  },
+
+  // Copy related messages
+  copy: {
+    success: "Copied to Clipboard",
+    failure: "Failed to Copy",
+  },
+
+  // Modifications to userInfo (name, descr, etc..)
+  userInfo: {
+    success: "User Info updated successfully, please wait a block to see the changes.",
+    failure: "Failed to Update User Info",
+  },
+};
