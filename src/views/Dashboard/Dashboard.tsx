e8acbe0cd922b0d0bc5afc3088590a90ac1e9ffd
outragedhuman <outragedhumangithub@mm.st>
2022-05-27 09:09:48 -0500

    add external nav links to sidebar/drawer

--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -2,10 +2,10 @@ import React, { memo } from "react";
 import { Box, Grid, Typography } from "@mui/material";
 import Page from "components/Page";
 import WidgetContainer from "./components/WidgetContainer";
-import Drawer from "../../components/Drawer";
 import SendWidget from "./components/Widgets/SendWidget";
 import TransactionsWidget from "./components/Widgets/TransactionsWidget";
 import SearchBar from "components/SearchBar";
+import Drawer from "components/Drawer";
 
 const PortfolioWidget: React.FC = () => {
   return (
