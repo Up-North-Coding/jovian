d6e5b69345909941f9f46c57bfcddc4f5e7e7e2f
outragedhuman <outragedhumangithub@mm.st>
2022-05-30 15:44:51 -0500

    review comment

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,7 @@ import { MaximumSnackbarMessages } from "utils/common/constants";
 render(
   <React.StrictMode>
     <CssBaseline />
+    {/* TODO: Is it possible to lower this in the app's heirarchy? It could lead to less un-neccesary renders */}
     <SnackbarProvider maxSnack={MaximumSnackbarMessages}>
       <App />
     </SnackbarProvider>
