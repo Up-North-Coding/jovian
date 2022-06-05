17120493c6af74220855e2294ca27ee8e35494f6
outragedhuman <outragedhumangithub@mm.st>
2022-06-05 16:31:07 -0500

    add mobile styling to blockheightchip

--- src/components/SearchBar/components/BlockheightChip/BlockheightChip.tsx
+++ src/components/SearchBar/components/BlockheightChip/BlockheightChip.tsx
@@ -1,10 +1,17 @@
 import React, { memo } from "react";
-import { Chip } from "@mui/material";
+import { Chip, styled } from "@mui/material";
 import useBlocks from "hooks/useBlocks";
+import useBreakpoint from "hooks/useBreakpoint";
 
 const BlockheightChip: React.FC = () => {
   const { blockHeight } = useBlocks();
+  const isMobileMedium = useBreakpoint("<", "md");
 
-  return <Chip color="primary" label={"Height: " + blockHeight} />;
+  return <StyledChip color="primary" label={isMobileMedium ? blockHeight : "Height: " + blockHeight} />;
 };
+
+const StyledChip = styled(Chip)(({ theme }) => ({
+  margin: "0px 10px",
+}));
+
 export default memo(BlockheightChip);
