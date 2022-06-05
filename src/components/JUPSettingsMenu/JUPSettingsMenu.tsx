83157e2c1c790ba935a34955c3015a255da7cc2a
outragedhuman <outragedhumangithub@mm.st>
2022-06-05 15:19:46 -0500

    move menu's paper styling into the component file

--- src/components/JUPSettingsMenu/JUPSettingsMenu.tsx
+++ src/components/JUPSettingsMenu/JUPSettingsMenu.tsx
@@ -6,11 +6,37 @@ interface IJUPSettingsMenu {
   anchorEl: HTMLElement | null;
   open: boolean;
   handleClose: () => void;
-  menuProps: any;
   handleClick: (event: React.MouseEvent<HTMLElement>) => void;
 }
 
-const JUPSettingsMenu: React.FC<IJUPSettingsMenu> = ({ handleClick, anchorEl, open, handleClose, menuProps }) => {
+const menuProps = {
+  elevation: 0,
+  sx: {
+    overflow: "visible",
+    filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+    mt: 1.5,
+    "& .MuiAvatar-root": {
+      width: 32,
+      height: 32,
+      ml: -0.5,
+      mr: 1,
+    },
+    "&:before": {
+      content: '""',
+      display: "block",
+      position: "absolute",
+      top: 0,
+      right: 14,
+      width: 10,
+      height: 10,
+      bgcolor: "background.paper",
+      transform: "translateY(-50%) rotate(45deg)",
+      zIndex: 0,
+    },
+  },
+};
+
+const JUPSettingsMenu: React.FC<IJUPSettingsMenu> = ({ handleClick, anchorEl, open, handleClose }) => {
   return (
     <>
       {" "}
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -9,33 +9,6 @@ import JUPSettingsMenu from "components/JUPSettingsMenu";
 
 const drawerWidth = 240;
 
-const menuProps = {
-  elevation: 0,
-  sx: {
-    overflow: "visible",
-    filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-    mt: 1.5,
-    "& .MuiAvatar-root": {
-      width: 32,
-      height: 32,
-      ml: -0.5,
-      mr: 1,
-    },
-    "&:before": {
-      content: '""',
-      display: "block",
-      position: "absolute",
-      top: 0,
-      right: 14,
-      width: 10,
-      height: 10,
-      bgcolor: "background.paper",
-      transform: "translateY(-50%) rotate(45deg)",
-      zIndex: 0,
-    },
-  },
-};
-
 // Placeholder values for the autocomplete
 const placeHolderVals = ["test", "hello"];
 
@@ -69,7 +42,7 @@ const SearchBar: React.FC = () => {
         />
         <AddressBook />
         <BlockheightChip />
-        <JUPSettingsMenu handleClick={handleClick} anchorEl={anchorEl} open={open} handleClose={handleClose} menuProps={menuProps}></JUPSettingsMenu>
+        <JUPSettingsMenu handleClick={handleClick} anchorEl={anchorEl} open={open} handleClose={handleClose}></JUPSettingsMenu>
       </SearchStack>
     </AppBar>
   );
