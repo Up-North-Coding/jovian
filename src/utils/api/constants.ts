ef0ce7da6878c752393697337a732eea8e5601bb
outragedhuman <outragedhumangithub@mm.st>
2022-06-09 19:37:09 -0500

    small proxy changes to support previewing the website on a LAN phone

--- src/utils/api/constants.ts
+++ src/utils/api/constants.ts
@@ -1,4 +1,5 @@
 // TODO: Make this configurable based on prod/dev run status
 export const BASEURL = "http://localhost:3080/nxt?"; // proxy runs on 3080 by default
+// export const BASEURL = "http://<insert dev pc IP for mobile testing>:3080/nxt?"; // proxy runs on 3080 by default (for public testing)
 
 export const BASEREQBODY = "=%2Fnxt&requestType="; // all req bodies start this way
