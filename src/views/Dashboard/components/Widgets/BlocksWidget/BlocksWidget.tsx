bb86a9422f59b9db5819093ab58a320d2127c435
outragedhuman <outragedhumangithub@mm.st>
2022-06-12 14:00:01 -0500

    componentize the passed in chips to reduce bloat in blocks widget, extend detailed dialog data, small cleanups

--- src/views/Dashboard/components/Widgets/BlocksWidget/BlocksWidget.tsx
+++ src/views/Dashboard/components/Widgets/BlocksWidget/BlocksWidget.tsx
@@ -1,21 +1,11 @@
 import React, { memo, useCallback, useMemo, useState } from "react";
-import { Chip, styled, Typography } from "@mui/material";
-import { JUPGenesisTimestamp, ShortUnitPrecision, userLocale } from "utils/common/constants";
+import { Link, Tab, Tabs } from "@mui/material";
+import { JUPGenesisTimestamp, userLocale } from "utils/common/constants";
 import JUPTable, { IHeadCellProps, ITableRow } from "components/JUPTable";
 import useBlocks from "hooks/useBlocks";
 import JUPDialog from "components/JUPDialog";
-
-const AvgBlockTimeDisplay: React.FC = () => {
-  const { avgBlockTime } = useBlocks();
-
-  return <AvgBlockTimeChip label={`AVG Block Time: ${avgBlockTime?.toFixed(ShortUnitPrecision)} sec`} />;
-};
-
-const DailyTransactionsDisplay: React.FC = () => {
-  const { dailyTxs } = useBlocks();
-
-  return <AvgTxChip label={`24 Hr Txs: ${dailyTxs}`} />;
-};
+import AvgBlockTimeDisplay from "./components/AvgBlockTime";
+import DailyTransactionsDisplay from "./components/DailyTransactionsDisplay";
 
 const blockOverviewHeaders: Array<IHeadCellProps> = [
   {
@@ -57,57 +47,79 @@ const blockOverviewHeaders: Array<IHeadCellProps> = [
 ];
 
 interface IBlockDetail {
-  height:number;
+  height: number;
   headers: Array<IHeadCellProps>;
-  rows: Array<ITableRow>
+  rows: Array<ITableRow>;
 }
 
 const BlocksWidget: React.FC = () => {
+  const [detailedDialogOpen, setDetailedDialogOpen] = useState(false);
+  const [blockDetail, setBlockDetail] = useState<IBlockDetail | undefined>(undefined);
   const { recentBlocks } = useBlocks();
 
-  const [detailedDialogOpen, setDetailedDialogOpen] = useState(false)
-  const [blockDetail, setBlockDetail] = useState<IBlockDetail | undefined>(undefined)
+  const handleOpenBlockDetail = useCallback(
+    (height: number) => {
+      const block = recentBlocks?.filter((x) => x.height === height)[0];
 
-  const handleOpenBlockDetail = useCallback((height:number)=>{
-    console.log('opening detailed block height dialog', height)
+      setDetailedDialogOpen(true);
+      setBlockDetail({
+        height,
+        headers: [
+          {
+            id: "col1",
+            label: "Name",
+            headAlignment: "center",
+            rowAlignment: "center",
+          },
+          {
+            id: "col2",
+            label: "Details",
+            headAlignment: "center",
+            rowAlignment: "center",
+          },
+        ],
+        rows: [
+          {
+            col1: "Block Height",
+            col2: height,
+          },
+          {
+            col1: "Block Signature",
+            col2: block?.blockSignature,
+          },
+          {
+            col1: "Transaction Count",
+            col2: block?.numberOfTransactions,
+          },
+          {
+            col1: "Generator",
+            col2: block?.generatorRS,
+          },
+          {
+            col1: "Generator Pubkey",
+            col2: block?.generatorPublicKey,
+          },
+          {
+            col1: "Prev Block Hash",
+            col2: block?.previousBlockHash,
+          },
+          {
+            col1: "Generation Signature",
+            col2: block?.generationSignature,
+          },
+          {
+            col1: "Version",
+            col2: block?.version,
+          },
+        ],
+      } as IBlockDetail);
+    },
+    [recentBlocks]
+  );
 
-    const block = recentBlocks?.filter(x => x.height === height)[0]
-
-    console.log('found block', block)
-
-    setDetailedDialogOpen(true)
-    setBlockDetail({
-      height,
-      headers: [
-        {
-          id: "col1",
-          label: "Name",
-          headAlignment: "center",
-          rowAlignment: "center",
-        },
-        {
-          id: "col2",
-          label: "Value",
-          headAlignment: "center",
-          rowAlignment: "center",
-        },
-      ],
-      rows: [
-        {
-          col1: "Block Height",
-          col2: height
-        },
-        {
-          col1: "Block Signature",
-          col2: block?.blockSignature
-        }
-      ]
-    } as IBlockDetail)
-  }, [recentBlocks])
-
-  const handleCloseDialog = useCallback(()=>{
-    setDetailedDialogOpen(false)
-  }, [])
+  const handleCloseDialog = useCallback(() => {
+    setDetailedDialogOpen(false);
+  }, []);
 
   const blockOverviewRows: Array<ITableRow> | undefined = useMemo(() => {
     console.log("transactions testing", recentBlocks);
@@ -119,14 +131,11 @@ const BlocksWidget: React.FC = () => {
       return {
         date: new Date(block.timestamp * 1000 + JUPGenesisTimestamp * 1000).toLocaleString(userLocale.localeStr, userLocale.options),
         blockHeight: block.height.toString(),
-        blockHeight_ui: (
-          <a onClick={() => handleOpenBlockDetail(block.height)}>
-          {block.height}
-          </a>
-          ),
+        blockHeight_ui: <Link onClick={() => handleOpenBlockDetail(block.height)}>{block.height}</Link>,
         txCount: block.numberOfTransactions.toString(),
         value: block.totalAmountNQT,
         generator: block.generatorRS,
+        // TODO: move to constants, this value fixes an upstream calculation bug in the base target values
         baseTarget: Math.round(parseInt(block.baseTarget) / 153722867 / 10) + " %",
       };
     });
@@ -134,17 +143,25 @@ const BlocksWidget: React.FC = () => {
 
   return (
     <>
+      {/* Dialog for block details */}
       <JUPDialog title={`Detailed overview for block: ${blockDetail?.height}`} isOpen={detailedDialogOpen} closeFn={handleCloseDialog}>
+        {/* TODO: for mobile use variant="fullWidth" */}
+        <Tabs value={0} centered>
+          <Tab label="Transaction Details" />
+          <Tab label="Block Details" />
+        </Tabs>
+
         <JUPTable
-        title={"Detailed View"}
-        path={"/blocks"}
-        headCells={blockDetail?.headers}
-        rows={blockDetail?.rows}
-        keyProp={"col1"}
-        defaultSortOrder={"asc"}
-      />
+          title={"Detailed View"}
+          path={"/blocks"}
+          headCells={blockDetail?.headers}
+          rows={blockDetail?.rows}
+          keyProp={"col1"}
+          defaultSortOrder={"asc"}
+        />
       </JUPDialog>
 
+      {/* Main recent blocks widget */}
       <JUPTable
         title={"Recent Blocks"}
         path={"/blocks"}
@@ -156,19 +173,5 @@ const BlocksWidget: React.FC = () => {
     </>
   );
 };
-
-const AvgBlockTimeChip = styled(Chip)(({ theme }) => ({
-  position: "relative",
-  left: "50%",
-  right: "50%",
-  margin: "0px 10px",
-}));
-
-const AvgTxChip = styled(Chip)(({ theme }) => ({
-  position: "relative",
-  left: "50%",
-  right: "50%",
-  margin: "0px 10px",
-}));
 
 export default memo(BlocksWidget);
