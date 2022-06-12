0f4e1387232970c36e00b7493b0b4e93312514f1
Vance Walsh <vance@vancewalsh.com>
2022-06-12 12:32:12 -0500

    adding example detailed view for block widget dialog

--- src/views/Dashboard/components/Widgets/BlocksWidget/BlocksWidget.tsx
+++ src/views/Dashboard/components/Widgets/BlocksWidget/BlocksWidget.tsx
@@ -1,8 +1,9 @@
-import React, { memo, useMemo } from "react";
-import { Chip, styled } from "@mui/material";
+import React, { memo, useCallback, useMemo, useState } from "react";
+import { Chip, styled, Typography } from "@mui/material";
 import { JUPGenesisTimestamp, ShortUnitPrecision, userLocale } from "utils/common/constants";
 import JUPTable, { IHeadCellProps, ITableRow } from "components/JUPTable";
 import useBlocks from "hooks/useBlocks";
+import JUPDialog from "components/JUPDialog";
 
 const AvgBlockTimeDisplay: React.FC = () => {
   const { avgBlockTime } = useBlocks();
@@ -16,9 +17,9 @@ const DailyTransactionsDisplay: React.FC = () => {
   return <AvgTxChip label={`24 Hr Txs: ${dailyTxs}`} />;
 };
 
-const headCells: Array<IHeadCellProps> = [
+const blockOverviewHeaders: Array<IHeadCellProps> = [
   {
-    id: "blockHeight",
+    id: "blockHeight_ui",
     label: "Block #",
     headAlignment: "center",
     rowAlignment: "center",
@@ -55,10 +56,60 @@ const headCells: Array<IHeadCellProps> = [
   },
 ];
 
+interface IBlockDetail {
+  height:number;
+  headers: Array<IHeadCellProps>;
+  rows: Array<ITableRow>
+}
+
 const BlocksWidget: React.FC = () => {
   const { recentBlocks } = useBlocks();
 
-  const blockRows: Array<ITableRow> | undefined = useMemo(() => {
+  const [detailedDialogOpen, setDetailedDialogOpen] = useState(false)
+  const [blockDetail, setBlockDetail] = useState<IBlockDetail | undefined>(undefined)
+
+  const handleOpenBlockDetail = useCallback((height:number)=>{
+    console.log('opening detailed block height dialog', height)
+
+    const block = recentBlocks?.filter(x => x.height === height)[0]
+
+    console.log('found block', block)
+
+    setDetailedDialogOpen(true)
+    setBlockDetail({
+      height,
+      headers: [
+        {
+          id: "col1",
+          label: "Name",
+          headAlignment: "center",
+          rowAlignment: "center",
+        },
+        {
+          id: "col2",
+          label: "Value",
+          headAlignment: "center",
+          rowAlignment: "center",
+        },
+      ],
+      rows: [
+        {
+          col1: "Block Height",
+          col2: height
+        },
+        {
+          col1: "Block Signature",
+          col2: block?.blockSignature
+        }
+      ]
+    } as IBlockDetail)
+  }, [recentBlocks])
+
+  const handleCloseDialog = useCallback(()=>{
+    setDetailedDialogOpen(false)
+  }, [])
+
+  const blockOverviewRows: Array<ITableRow> | undefined = useMemo(() => {
     console.log("transactions testing", recentBlocks);
     if (recentBlocks === undefined || !Array.isArray(recentBlocks)) {
       return undefined;
@@ -68,21 +119,37 @@ const BlocksWidget: React.FC = () => {
       return {
         date: new Date(block.timestamp * 1000 + JUPGenesisTimestamp * 1000).toLocaleString(userLocale.localeStr, userLocale.options),
         blockHeight: block.height.toString(),
+        blockHeight_ui: (
+          <a onClick={() => handleOpenBlockDetail(block.height)}>
+          {block.height}
+          </a>
+          ),
         txCount: block.numberOfTransactions.toString(),
         value: block.totalAmountNQT,
         generator: block.generatorRS,
         baseTarget: Math.round(parseInt(block.baseTarget) / 153722867 / 10) + " %",
       };
     });
-  }, [recentBlocks]);
+  }, [recentBlocks, handleOpenBlockDetail]);
 
   return (
     <>
+      <JUPDialog title={`Detailed overview for block: ${blockDetail?.height}`} isOpen={detailedDialogOpen} closeFn={handleCloseDialog}>
+        <JUPTable
+        title={"Detailed View"}
+        path={"/blocks"}
+        headCells={blockDetail?.headers}
+        rows={blockDetail?.rows}
+        keyProp={"col1"}
+        defaultSortOrder={"asc"}
+      />
+      </JUPDialog>
+
       <JUPTable
         title={"Recent Blocks"}
         path={"/blocks"}
-        headCells={headCells}
-        rows={blockRows}
+        headCells={blockOverviewHeaders}
+        rows={blockOverviewRows}
         keyProp={"blockHeight"}
         DisplayedComponents={[<AvgBlockTimeDisplay key={"avg-block-time-display"} />, <DailyTransactionsDisplay key={"avg-tx-display"} />]}
       ></JUPTable>
