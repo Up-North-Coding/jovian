825cdf69916ba688846110e270deb1e465297498
outragedhuman <outragedhumangithub@mm.st>
2022-06-12 10:09:08 -0500

    commit constants file

---
+++ src/views/Dashboard/components/Widgets/BlocksWidget/data/headCells.tsx
@@ -0,0 +1,40 @@
+import { IHeadCellProps } from "components/JUPTable";
+
+export const headCells: Array<IHeadCellProps> = [
+  {
+    id: "blockHeight_ui",
+    label: "Block #",
+    headAlignment: "center",
+    rowAlignment: "center",
+  },
+  {
+    id: "date",
+    label: "Date",
+    headAlignment: "center",
+    rowAlignment: "center",
+  },
+  {
+    id: "txCount",
+    label: "Tx Qty",
+    headAlignment: "center",
+    rowAlignment: "center",
+  },
+  {
+    id: "value",
+    label: "Value",
+    headAlignment: "center",
+    rowAlignment: "center",
+  },
+  {
+    id: "generator",
+    label: "Generator",
+    headAlignment: "center",
+    rowAlignment: "center",
+  },
+  {
+    id: "baseTarget",
+    label: "Base Target",
+    headAlignment: "center",
+    rowAlignment: "center",
+  },
+];
