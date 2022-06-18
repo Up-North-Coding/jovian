546cfa654ebe542358826e9f73997bffbfdd445b
outragedhuman <outragedhumangithub@mm.st>
2022-06-18 14:31:08 -0500

    add some new constants and do some re-arranging

--- src/utils/common/constants.ts
+++ src/utils/common/constants.ts
@@ -2,23 +2,25 @@
 // The best place to store parameters which aren't yet exposed to the user but which we may want to expose
 //
 
-// Used to calculate the origination time of new transactions
-export const JUPGenesisTimestamp = 1508627969; // can be found in getConstants() API call as "epochBeginning"
+// TODO: implement some of these items as advanced features?
 
-// TODO: implement as advanced features?
+// Transaction stuff
+export const JUPGenesisTimestamp = 1508627969; // can be found in getConstants() API call as "epochBeginning", used to calculate the origination time of new transactions
 export const standardFee = "5000"; // may need adjustment for different tx types, but for now everything is working with this fee
 export const standardDeadline = 1440; // deadline for confirmation, required in most (all?) txs
 export const standardTimestamp = Math.round(Date.now() / 1000) - JUPGenesisTimestamp; // Seconds since Genesis. sets the origination time of the tx (since broadcast can happen later).
 export const standardTransactionVersion = 1; // Havn't seen anything but version 1 thus far
 export const standardPhasedSetting = false; // we don't support phased tx's
 export const standardECBlockheight = 0; // 0 seems like the safest choice here
-export const userLocale = { localeStr: "en-US", options: { timeZone: "America/Chicago" } }; // CST for testing, controls date/time stamps for human readability
-export const LongUnitPrecision = 8; // 8 digits of unit precision, used broadly
-export const ShortUnitPrecision = 2; // 2 digits of unit precision, used broadly
+export const AssetTransferType = 2;
+export const AssetTransferSubType = 1;
 
 // Gneral stuff
 export const JUPSidebarWidth = 260; // controls the width of the sidebar nav/details
 export const JUPSidebarMiniWidth = 100; // controls the width of the sidebar in mobile mode
+export const userLocale = { localeStr: "en-US", options: { timeZone: "America/Chicago" } }; // CST for testing, controls date/time stamps for human readability
+export const LongUnitPrecision = 8; // 8 digits of unit precision, used broadly
+export const ShortUnitPrecision = 2; // 2 digits of unit precision, used broadly
 
 //  Table stuff
 export const TableRowsPerPageOptions = [3, 5]; // Which row count options should be displayed in tables
