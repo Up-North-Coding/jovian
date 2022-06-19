1e3673d8f9d39c957309de1b7baf810ff30320af
outragedhuman <outragedhumangithub@mm.st>
2022-06-19 14:13:28 -0500

    partially complete sendAsset feature

--- src/views/Dashboard/components/Widgets/PortfolioWidget/PortfolioWidget.tsx
+++ src/views/Dashboard/components/Widgets/PortfolioWidget/PortfolioWidget.tsx
@@ -1,9 +1,18 @@
-import React, { memo, useCallback, useEffect, useMemo } from "react";
+import React, { memo, useCallback, useEffect, useMemo, useState } from "react";
 import JUPTable, { IHeadCellProps, ITableRow } from "components/JUPTable";
-import { Button, Stack } from "@mui/material";
+import { Box, Button, DialogContent, Input, Stack, styled, Typography } from "@mui/material";
 import useAssets from "hooks/useAssets";
 import { useSnackbar } from "notistack";
 import { messageText } from "utils/common/messages";
+import { IUnsignedTransaction } from "types/NXTAPI";
+import { buildTx } from "utils/common/txBuilder";
+import useAccount from "hooks/useAccount";
+import useAPI from "hooks/useAPI";
+import { AssetTransferSubType, AssetTransferType } from "utils/common/constants";
+import JUPDialog from "components/JUPDialog";
+
+const HARDCODEDVALUE = "0";
+const HARDCODEDASSETQTY = "1";
 
 const headCells: Array<IHeadCellProps> = [
   {
@@ -33,6 +42,11 @@ const headCells: Array<IHeadCellProps> = [
 ];
 
 const PortfolioWidget: React.FC = () => {
+  const [assetToSend, setAssetToSend] = useState<string>();
+  const [requestUserSecret, setRequestUserSecret] = useState<boolean>(false);
+  const [userSecretInput, setUserSecretInput] = useState<string>("");
+  const { accountRs, publicKey } = useAccount();
+  const { getAccount, getAccountId, sendJUP } = useAPI();
   const { heldAssets } = useAssets();
   const { enqueueSnackbar } = useSnackbar();
 
@@ -43,6 +57,84 @@ const PortfolioWidget: React.FC = () => {
     },
     [enqueueSnackbar]
   );
+
+  const fetchRecipAccountId = useCallback(async () => {
+    if (getAccount !== undefined && getAccountId !== undefined) {
+      try {
+        const result = await getAccount(HARDCODEDSENDASSETADDRESS);
+        if (result) {
+          const accountResult = await getAccountId(result.publicKey);
+          if (accountResult) {
+            return accountResult.account;
+          }
+        }
+      } catch (e) {
+        console.error("error while fetching public key:", e);
+        return;
+      }
+    }
+  }, [getAccount, getAccountId]);
+
+  const handleSendAsset = useCallback((assetId: string) => {
+    setAssetToSend(assetId);
+    setRequestUserSecret(true);
+  }, []);
+
+  const prepareUnsignedTx = useCallback(
+    async (secret: string) => {
+      const recipientAccountId = await fetchRecipAccountId();
+      const tx: IUnsignedTransaction = buildTx({
+        senderPubKey: publicKey, // publicKey from useAccount() hook
+        senderRS: accountRs, // accountRs from useAccount() hook
+        type: AssetTransferType,
+        subtype: AssetTransferSubType,
+        attachment: { "version.AssetTransfer": 1, quantityQNT: HARDCODEDASSETQTY, asset: assetToSend },
+        amountNQT: HARDCODEDVALUE, // TODO: use converter function, but for now it's nice for cheaper testing
+        recipientRS: HARDCODEDSENDASSETADDRESS,
+        recipient: recipientAccountId,
+        secretPhrase: secret,
+      });
+
+      // const unsignedTx = await prepareUnsignedTx(secret);
+      if (sendJUP !== undefined && tx !== undefined) {
+        const result = await sendJUP(tx);
+        if (result) {
+          enqueueSnackbar(messageText.transaction.success, { variant: "success" });
+          return;
+        }
+        enqueueSnackbar(messageText.transaction.failure, { variant: "error" });
+        console.log("send result:", result);
+      }
+
+      console.log("built tx: ", tx);
+    },
+    [accountRs, assetToSend, enqueueSnackbar, fetchRecipAccountId, publicKey, sendJUP]
+  );
+
+  const handleSubmitSecret = useCallback(
+    async (secret: string) => {
+      const unsignedTx = await prepareUnsignedTx(secret);
+      if (sendJUP !== undefined && unsignedTx !== undefined) {
+        const result = await sendJUP(unsignedTx);
+        if (result) {
+          enqueueSnackbar(messageText.transaction.success, { variant: "success" });
+          return;
+        }
+        enqueueSnackbar(messageText.transaction.failure, { variant: "error" });
+        console.log("send result:", result);
+      }
+    },
+    [enqueueSnackbar, prepareUnsignedTx, sendJUP]
+  );
+
+  const handleSecretEntry = useCallback((secretInput) => {
+    setUserSecretInput(secretInput);
+  }, []);
+
+  const handleCloseSeedCollection = useCallback(() => {
+    setRequestUserSecret(false);
+    enqueueSnackbar(messageText.transaction.cancel, { variant: "warning" });
+  }, [enqueueSnackbar]);
 
   const portfolioRows: Array<ITableRow> | undefined = useMemo(() => {
     if (heldAssets === undefined || !Array.isArray(heldAssets)) {
@@ -57,7 +149,7 @@ const PortfolioWidget: React.FC = () => {
         assetDescription: asset.description,
         actions: (
           <Stack direction={"row"} spacing={2} justifyContent="center">
-            <Button variant="green" onClick={() => console.log("implement send")}>
+            <Button variant="green" onClick={() => handleSendAsset(asset.asset)}>
               Send
             </Button>
             <Button variant="green" onClick={() => handleCopyAssetId(asset.asset)}>
@@ -67,18 +159,52 @@ const PortfolioWidget: React.FC = () => {
         ),
       };
     });
-  }, [handleCopyAssetId, heldAssets]);
+  }, [handleCopyAssetId, handleSendAsset, heldAssets]);
+
+  const ConditionalSendAssetMemo = useMemo(() => {
+    return (
+      requestUserSecret && (
+        <>
+          <JUPDialog isOpen={requestUserSecret} closeFn={handleCloseSeedCollection}>
+            <DialogContent>
+              <Box sx={{ minWidth: "600px", height: "300px" }}>
+                <Typography align="center">Please enter your seed phrase.</Typography>
+                <Stack sx={{ alignItems: "center" }}>
+                  <SeedphraseEntryBox onChange={(e) => handleSecretEntry(e.target.value)} type="password" placeholder="Enter Seed Phrase" />
+                  <ConfirmButton variant="contained" onClick={() => handleSubmitSecret(userSecretInput)}>
+                    Confirm & Send
+                  </ConfirmButton>
+                </Stack>
+              </Box>
+            </DialogContent>
+          </JUPDialog>
+        </>
+      )
+    );
+  }, [handleCloseSeedCollection, handleSecretEntry, handleSubmitSecret, requestUserSecret, userSecretInput]);
 
   return (
-    <JUPTable
-      title={"My Portfolio"}
-      path={"/portfolio"}
-      headCells={headCells}
-      rows={portfolioRows}
-      defaultSortOrder="asc"
-      keyProp={"assetId"}
-    ></JUPTable>
+    <>
+      <JUPTable
+        title={"My Portfolio"}
+        path={"/portfolio"}
+        headCells={headCells}
+        rows={portfolioRows}
+        defaultSortOrder="asc"
+        keyProp={"assetId"}
+      ></JUPTable>
+      {ConditionalSendAssetMemo}
+    </>
   );
 };
+
+const SeedphraseEntryBox = styled(Input)(({ theme }) => ({
+  minWidth: "400px",
+  margin: "40px 0px",
+}));
+
+const ConfirmButton = styled(Button)(({ theme }) => ({
+  margin: "20px 0px",
+}));
 
 export default memo(PortfolioWidget);
