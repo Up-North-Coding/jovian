41f44f22eaee64a330619225b2bdccafaf653ce2
outragedhuman <outragedhumangithub@mm.st>
2022-08-07 15:12:55 -0500

    fix "any" in sendJUP

--- src/utils/api/sendJUP.ts
+++ src/utils/api/sendJUP.ts
@@ -16,7 +16,7 @@ export interface ISignTransactionPayload extends IAPICall {
 
 export interface IBroadcastTransactionPayload extends IAPICall {
   data: {
-    transactionJSON: any;
+    transactionJSON: ISignedTransaction;
   };
 }
 
