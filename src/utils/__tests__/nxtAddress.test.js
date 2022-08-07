dd4ae1eb283e9276d6cf626fb7cf6d42118e0ac1
outragedhuman <outragedhumangithub@mm.st>
2022-08-07 15:41:21 -0500

    remove unneeded test

--- src/utils/__tests__/nxtAddress.test.js
+++ src/utils/__tests__/nxtAddress.test.js
@@ -7,15 +7,3 @@ beforeEach(() => {
 it("should create a new instance", () => {
   new NxtAddress();
 });
-
-// TODO: improve this test or discard it if it's not needed, it might not be doing much right now
-it("should set a valid accountID without error", () => {
-  const testAccountId = "test";
-  const address = new NxtAddress();
-
-  if (address.set(testAccountId)) {
-    expect(address.toString()).not.toBe("");
-  } else {
-    expect(address.toString()).toBe("JUP-3333-3333-3333-33333");
-  }
-});
