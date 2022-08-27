2e61f49d71e14f077510b5d41702e42cb15192d2
outragedhuman <outragedhumangithub@mm.st>
2022-08-27 10:06:40 -0500

    standardize view

--- src/views/Transactions/Transactions.tsx
+++ src/views/Transactions/Transactions.tsx
@@ -1,16 +1,38 @@
-import React, { memo } from "react";
+import React, { memo, useCallback, useEffect, useState } from "react";
 import { Typography } from "@mui/material";
 import Page from "components/Page";
 import Drawer from "../../components/Drawer";
 import SearchBar from "components/SearchBar";
+import useBreakpoint from "hooks/useBreakpoint";
+import WidgetContainer from "views/Dashboard/components/WidgetContainer";
 
 const Transactions: React.FC = () => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState<boolean>(true);
+  const isMobileLarge = useBreakpoint("<", "lg");
+  const isMobileSmall = useBreakpoint("<", "sm");
+  const gridSize = isMobileLarge ? 12 : 6; // switch from double-column to single-column for smaller screens
+
+  const handleDrawerToggle = useCallback(() => {
+    setDrawerIsOpen((prev: boolean) => !prev);
+  }, []);
+
+  // sets the drawer state when the mobile breakpoint is hit
+  useEffect(() => {
+    if (isMobileSmall) {
+      setDrawerIsOpen(false);
+      return;
+    }
+    setDrawerIsOpen(true);
+  }, [isMobileSmall]);
+
   return (
     <Page>
-      {/* True temporarily pased in here, eventually Drawer will be hoisted to the <Page> level */}
-      <Drawer isSidebarExpanded={true} />
-      <SearchBar />
-      <Typography>Placeholder for the full Transactions page, coming soon.</Typography>
+      <WidgetContainer isSidebarExpanded={drawerIsOpen}>
+        {/* True temporarily pased in here, eventually Drawer will be hoisted to the <Page> level */}
+        <Drawer isSidebarExpanded={true} />
+        <SearchBar />
+        <Typography>Placeholder for the full Transactions page, coming soon.</Typography>
+      </WidgetContainer>
     </Page>
   );
 };
